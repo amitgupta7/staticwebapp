2f1b1ff9305d6443f20a450cec8aeceb64cbedb3
amitgupta7 <amit864@gmail.com>
2022-11-23 11:20:07 +0530

    adding tracking cookie with expires

--- src/tracker.js
+++ src/tracker.js
@@ -1,6 +1,12 @@
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
 let params = (new URL(document.location)).searchParams;
 let name = params.get("name");
-document.cookie = `tracking=${document.location}`
+setCookie("tracking", `${document.location.toString()}`, 1000);
 (async function() {
     apiRep = await( await fetch(`/api/message?name=${name}`)).json();
     document.querySelector('#message').textContent = apiRep.message;
